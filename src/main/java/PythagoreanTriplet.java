b99779bef038c6e546fe83d723a7a67299edbc31
ramr161189 <ramprasad@devathon.com>
2022-02-12 21:31:51 +0530

    Pythagorean-triplets

---
+++ src/main/java/PythagoreanTriplet.java
@@ -0,0 +1,18 @@
+/*
+
+Since this exercise has a difficulty of > 4 it doesn't come
+with any starter implementation.
+This is so that you get to practice creating classes and methods
+which is an important part of programming in Java.
+
+Please remove this comment when submitting your solution.
+
+*/
+class PythagoreanTriplet
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello");
+    }
+}
+    
